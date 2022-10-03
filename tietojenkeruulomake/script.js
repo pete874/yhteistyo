cc9b12e279261aecb4932306d41552439ec12d6e
Petri Nurmi <pete874@hotmail.com>
2022-10-03 14:56:44 +0300

    .

--- tietojenkeruulomake/script.js
+++ tietojenkeruulomake/script.js
@@ -6,12 +6,23 @@ function tarkasta(form) {
   // Tallennetaan jokaisen inputin arvo omiin muuttujiin:
 
   var etunimi = form.enimi.value;
+  var sukunimi = form.snimi.value;
+  var email = form.email.value;
+  var textarea = form.textarea.value;
 
   //  Tarkistetaan etunimen pituus ja palautetaan false, jotta sivu ei siirry "kiitos" sivulle, jos nimi ei ole tarpeeksi pitkä.
 
   if (etunimi.length < 4) {
-    alert("anna pidempi kuin 4 merkkinen nimi")
+    alert("anna pidempi kuin 4 merkkinen etunimi")
     form.enimi.focus();
+    return false;
+  }
+
+  // Tarkistetaan sukunimen pituus:
+
+  if (sukunimi.length < 4) {
+    alert("anna pidempi kuin 4 merkkinen sukunimi")
+    form.snimi.focus();
     return false;
   }
 }
