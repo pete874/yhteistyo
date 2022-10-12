f6fd559ac27ae128fb73c30e120a4a72dc9454ee
Ville Saarinen <ville.saarinen@edu.keuda.fi>
2022-10-12 10:54:44 +0300

    Projekti 2B Ville

---
+++ 5 kysymystä/Ville/js/script.js
@@ -0,0 +1,33 @@
+function lahetaVastaukset() {
+
+  var yhteensa = 5;
+  var pisteet = 0; // Alustetaan yhteispisteet nollaksi
+  var huom = 0;
+  // Haetaan käyttäjän syötteet
+  var k1 = document.forms["kyselyLomake"]["k1"].value;
+  var k2 = document.forms["kyselyLomake"]["k2"].value;
+  var k3 = document.forms["kyselyLomake"]["k3"].value;
+  var k4 = document.forms["kyselyLomake"]["k4"].value;
+  var k5 = document.forms["kyselyLomake"]["k5"].value;
+  // Tarkastus
+  for(i = 1; i <= yhteensa; i++) {
+    if(eval('k' + i) == null || eval('k' + i) == '') {
+      alert('Et vastannut kysymykseen numero: ' + i);
+      huom = 1;
+    }
+    if(huom == 1) return false;
+  }
+  // Asetetaan oikeat vastaukset
+  var vastaukset = ["b", "a", "d", "b", "d"];
+  // Tarkastetaan oikeat vastaukset
+  for(i = 1; i <= yhteensa; i++) {
+    if(eval('k' + i) == vastaukset[i - 1]) {
+      pisteet++;
+    }
+  }
+  // Näytetään vastaukset
+  var tulokset = document.getElementById("tulokset");
+  alert('Sait ' + pisteet + ' pistettä, kun maksimi pistemäärä oli ' + yhteensa);
+  tulokset.innerHTML = '<h3>Sait <span>' + pisteet + '</span> pistettä, kun maksimi pistemäärä oli <span>' + yhteensa + '</span></h3>';
+  return false;
+}
