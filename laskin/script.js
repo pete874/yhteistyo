a3ce1f4475ed32d03638d607b44b72ad6ccfc036
Petri Nurmi <pete874@hotmail.com>
2022-10-13 11:04:12 +0300

    .

--- laskin/script.js
+++ laskin/script.js
@@ -3,33 +3,22 @@ var luku = "";
 
 
 function lisays(luku) {
-  if (numero.length == 12) {
-    numero += "<br>";
-  }
-  if (numero.length == 28) {
-    numero += "<br>"
-  }
-  if (numero.length == 44) {
-    numero += "<br>"
-  }
-  if (numero.length > 59) {
-    alert("Tila loppui kesken")
-  } else {
-    numero += luku;
-  }
 
-  document.getElementById("naytto").innerHTML = numero;
+    numero += luku;
+
+
+  document.getElementById("naytto").value = numero;
 }
 
 function tyhjennys() {
 
   numero = "";
-  document.getElementById("naytto").innerHTML = numero;
+  document.getElementById("naytto").value = numero;
 }
 
 function yht() {
 
-    document.getElementById('naytto').innerHTML = eval(numero);
+    document.getElementById('naytto').value = eval(numero);
     numero = eval(numero);
 }
 
@@ -37,7 +26,7 @@ function poisto() {
   if (numero.length >= 0) {
     numero = numero.replace(/\d+$/,'');
 
-    document.getElementById('naytto').innerHTML = numero;
+    document.getElementById('naytto').value = numero;
 
   }
 }
