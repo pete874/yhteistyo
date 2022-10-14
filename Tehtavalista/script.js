fe74efe9608ed80b5dea1279656c9e0cc7a1b8e0
Petri Nurmi <pete874@hotmail.com>
2022-10-14 13:18:51 +0300

    Poistonappi lisätty ja enter toiminnallisuus

--- Tehtavalista/script.js
+++ Tehtavalista/script.js
@@ -1,17 +1,50 @@
 
-
-
-
 // Tehdään funktio listaan lisäystä varten
 
 function lisaa() {
   //haetaan text inputin value muuttujaan
   var tehtava = document.getElementById('linta_input').value;
+
+  // Tehdään muuttujat ul ja li elementeille
   var ul = document.getElementById('lista_ul');
   var li = document.createElement("li");
 
+
+  // Muuttuja joka luo tekstisisällön tehtava muuttujan sisällön mukaan
   var taulukkoon = document.createTextNode(tehtava);
   li.appendChild(taulukkoon);
   ul.appendChild(li);
 
+
+  //Lisätään poistonappi listaelementtin
+  var poistoNappi = document.createElement("button")
+
+  // Lisätään poistonapille ja li elementille id, jotta niitä voidaan muokata CSS:ssä
+  poistoNappi.setAttribute("class", "poistonappi")
+  li.setAttribute("class", "lista_item")
+
+  // Lisätään napille onclick attribuutti, joka poistaa parent elementin, eli <li> elementin
+  poistoNappi.setAttribute("onclick", "parentElement.remove();")
+
+  // Lisätään napille tekstisisältö
+  poistoNappi.textContent = "X";
+
+  // Lisätään poistonappi <li> elementin alle
+  li.appendChild(poistoNappi);
+
+
 }
+
+//lisätään enter napin toiminnallisuus eventlistenerillä
+
+//haetaan muuttumaan input jonka halutaan reagoivan napin painallukseen
+var enter = document.getElementById('linta_input');
+
+//lisätään inputille eventlistener, joka kuuntelee napin painallusta kyseisen inputin kohdalla
+enter.addEventListener("keypress", function(enterNappi) {
+  //määritetään nappi jonka painallusta halutaan kuunnella
+  if (enterNappi.key === "Enter") {
+    //määritetään asiat jotka halutaan tapahtuvan napin painalluksen jälkeen, eli buttonin clickkaus tässä tapauksessa
+    document.getElementById('lista_button').click();
+  }
+})
