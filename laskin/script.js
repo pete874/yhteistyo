f1789ed66adf7f071d3392d22569f0d9f3b40e60
Ville Saarinen <ville.saarinen@edu.keuda.fi>
2022-10-26 16:44:25 +0300

    oma näyttö muistille, erikoismerkkien disabloiminen

--- laskin/script.js
+++ laskin/script.js
@@ -1,26 +1,63 @@
 var numero = "";
 var luku = "";
 
+ document.getElementById('jakoj').disabled = true;
+ document.getElementById('jako').disabled = true;
+ document.getElementById('kerto').disabled = true;
+ document.getElementById('miinus').disabled = true;
+ document.getElementById('plus').disabled = true;
+ document.getElementById('piste').disabled = true;
+ document.getElementById('enter').disabled = true;
 
 function lisays(luku) {
   numero += luku;
-  document.getElementById("naytto").value = numero;
+  var test = /[0-9]/;
+
+  if(test.test(luku) == true) {
+    document.getElementById('jakoj').disabled = false;
+    document.getElementById('jako').disabled = false;
+    document.getElementById('kerto').disabled = false;
+    document.getElementById('miinus').disabled = false;
+    document.getElementById('plus').disabled = false;
+    document.getElementById('piste').disabled = false;
+    document.getElementById('enter').disabled = false;
+  }
+
+  document.getElementById("naytto_1").value = numero;
 }
 
 function yht() {
-  document.getElementById('naytto').value = eval(numero);
-  numero = eval(numero);
+  document.getElementById('jakoj').disabled = true;
+  document.getElementById('jako').disabled = true;
+  document.getElementById('kerto').disabled = true;
+  document.getElementById('miinus').disabled = true;
+  document.getElementById('plus').disabled = true;
+  document.getElementById('piste').disabled = true;
+  document.getElementById('enter').disabled = true;
 
+  document.getElementById('naytto_2').value += numero + "=" + "\r\n" + eval(numero) + "\r\n";
+  numero = eval(numero);
+  numero = "";
+  document.getElementById('naytto_1').value = numero;
 }
 
 function poisto() {
   if (numero.length >= 0) {
   numero = numero.replace(/\d+$/,'');
-  document.getElementById('naytto').value = numero;
+  document.getElementById('naytto_1').value = numero;
   }
 }
 
 function tyhjennys() {
+  document.getElementById('jakoj').disabled = true;
+  document.getElementById('jako').disabled = true;
+  document.getElementById('kerto').disabled = true;
+  document.getElementById('miinus').disabled = true;
+  document.getElementById('plus').disabled = true;
+  document.getElementById('piste').disabled = true;
+  document.getElementById('enter').disabled = true;
+  
   numero = "";
-  document.getElementById("naytto").value = numero;
+  document.getElementById("naytto_1").value = numero;
+  document.getElementById('naytto_2').value = numero;
 }
