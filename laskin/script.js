83f95d838abe4631066c57feaead7a2008b82c2c
Ville Saarinen <ville.saarinen@edu.keuda.fi>
2022-10-26 12:55:22 +0300

    Laskimen tyhjennysnappien uudelleen nimeäminen C / CE

--- laskin/script.js
+++ laskin/script.js
@@ -3,30 +3,24 @@ var luku = "";
 
 
 function lisays(luku) {
-
-    numero += luku;
-
-
-  document.getElementById("naytto").value = numero;
-}
-
-function tyhjennys() {
-
-  numero = "";
+  numero += luku;
   document.getElementById("naytto").value = numero;
 }
 
 function yht() {
+  document.getElementById('naytto').value = eval(numero);
+  numero = eval(numero);
 
-    document.getElementById('naytto').value = eval(numero);
-    numero = eval(numero);
 }
 
 function poisto() {
   if (numero.length >= 0) {
-    numero = numero.replace(/\d+$/,'');
-
-    document.getElementById('naytto').value = numero;
-
+  numero = numero.replace(/\d+$/,'');
+  document.getElementById('naytto').value = numero;
   }
+}
+
+function tyhjennys() {
+  numero = "";
+  document.getElementById("naytto").value = numero;
 }
