bee5bac4101e3e7f0316cb2d35c578385cd4685b
Ville Saarinen <ville.saarinen@edu.keuda.fi>
2022-10-28 12:12:41 +0300

    Kommentointia

--- laskin/script.js
+++ laskin/script.js
@@ -1,6 +1,8 @@
+// Luodaan globaalit numero ja luku-muuttujat
+
 var numero = "";
 var luku = "";
-
+// Haetaan laskuoperaattorit ja poistetaan ne lähtökohtaisesti käytöstä
  document.getElementById('jakoj').disabled = true;
  document.getElementById('jako').disabled = true;
  document.getElementById('kerto').disabled = true;
@@ -8,9 +10,10 @@ var luku = "";
  document.getElementById('plus').disabled = true;
  document.getElementById('piste').disabled = true;
  document.getElementById('enter').disabled = true;
-
+// Syöttöfunktio
 function lisays(luku) {
   numero += luku;
+// Määritetään, että numerosyötön jälkeen laskuoperaattorit ovat käytössä
   var test = /[0-9]/;
 
   if(test.test(luku) == true) {
@@ -22,24 +25,28 @@ function lisays(luku) {
     document.getElementById('piste').disabled = false;
     document.getElementById('enter').disabled = false;
   }
-
+// Viedään käyttäjän syöttö näytölle
   document.getElementById("naytto_1").value = numero;
 }
-
+// Laskutoimitus funktio
 function yht() {
+// Syötetään kakkosnäytölle laskutoimitus ja lopputulos
   document.getElementById('naytto_2').value += numero + "=" + "\r\n" + eval(numero) + "\r\n";
+// Syötetään ykkösnäytölle pelkkä lopputulos
   numero = eval(numero);
   document.getElementById('naytto_1').value = numero;
 }
-
+// Poistofunktio
 function poisto() {
+// Mikäli näytöllä useampi kuin 0 numeroa, funktio poistaa viimeisen syötetyn merkin
   if (numero.length >= 0) {
   numero = numero.slice(0, -1);
   document.getElementById('naytto_1').value = numero;
   }
 }
-
+// Tyhjennysfunktio
 function tyhjennys() {
+// Tyhjennyksen jälken laskuoperaattorit ovat taas pois käytöstä
   document.getElementById('jakoj').disabled = true;
   document.getElementById('jako').disabled = true;
   document.getElementById('kerto').disabled = true;
@@ -47,7 +54,7 @@ function tyhjennys() {
   document.getElementById('plus').disabled = true;
   document.getElementById('piste').disabled = true;
   document.getElementById('enter').disabled = true;
-
+// Tyhjennetään numeromuuttuja
   numero = "";
   document.getElementById("naytto_1").value = numero;
   document.getElementById('naytto_2').value = numero;
