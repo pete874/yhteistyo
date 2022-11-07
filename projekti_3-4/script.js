d3f252fd7f4f49236258b4f01ec6d40b2dadaeb7
Petri Nurmi <pete874@hotmail.com>
2022-11-07 17:21:28 +0200

    Javascript elementtien liikuttelu testiä

--- projekti_3-4/script.js
+++ projekti_3-4/script.js
@@ -2,13 +2,23 @@
 // Piilottaa headerin alaspäin scrollatessa ja paljastaa sen ylöspäin scrollatessa
 var headerEdellinenPaikka = window.pageYOffset;
 window.onscroll = function() {
-var headerNykyPaikka = window.pageYOffset;
-  if (headerEdellinenPaikka > headerNykyPaikka) {
-    document.getElementById("header_id").style.top = "0";
-  } else {
-    document.getElementById("header_id").style.top = "-100px";
-  }
-  headerEdellinenPaikka = headerNykyPaikka;
+  console.log(window.pageYOffset)
+
+  var section2 = document.getElementById('article_1');
+  var section2Offset = window.pageYOffset * 2.36;
+  var section3 = document.getElementById('article_2');
+  var section3Offset = window.pageYOffset * 1.45;
+
+  section2.style.right = (section2Offset - 2600) + "px";
+  section3.style.left = (section3Offset - 2610) + "px";
+
+  var headerNykyPaikka = window.pageYOffset;
+    if (headerEdellinenPaikka > headerNykyPaikka) {
+      document.getElementById("header_id").style.top = "0";
+    } else {
+      document.getElementById("header_id").style.top = "-100px";
+    }
+    headerEdellinenPaikka = headerNykyPaikka;
 }
 
 // Ilmottaumis div esiin ja pois klikkaamalla ilmottautuminen linkkiä headerissä
