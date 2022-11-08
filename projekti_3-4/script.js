f968af70e9c1e3b8a667f663c8b54f7b1950e162
SaarinenVille <ville.saarinen@edu.keuda.fi>
2022-11-08 20:07:07 +0200

    Värikokeiluja (käytin vain ilmoittautumisnapin taustaväriä, sekä mustaa ja valkoista shadowhin)
    Headerin kavennus ja väri/efektin vaihto klikattaessa.
    
    Fontit articleihin

--- projekti_3-4/script.js
+++ projekti_3-4/script.js
@@ -13,7 +13,7 @@ window.onscroll = function() {
 
   //outro titlen liikuttaminen
   var outroTitle = document.getElementById('outro_title')
-  var outroTitleOffset = window.pageYOffset - 2280;
+  var outroTitleOffset = window.pageYOffset - 2532;
   outroTitle.style.right = outroTitleOffset * 2 + "px";
 
   //artikkelien liikutus
@@ -24,21 +24,22 @@ window.onscroll = function() {
 
   if(window.pageYOffset < 1100) {
   article2.style.right = (article2Offset - 2600) + "px";}
-  if(window.pageYOffset < 1793) {
-  article3.style.left = (article3Offset - 4220) + "px";}
+  if(window.pageYOffset < 1693) {
+  article3.style.left = (article3Offset - 3960) + "px";}
 
+  //sectioneiden supistus
   var section2 = document.getElementById('section_2');
   var section3 = document.getElementById('section_3');
   var section2Offset = - window.pageYOffset / 0.8;
   var section3Offset = - window.pageYOffset / 0.8;
   console.log(section2Offset)
 
-  if(window.pageYOffset > 1000 && window.pageYOffset < 1660) {
+  if(window.pageYOffset > 1000 && window.pageYOffset < 1558) {
     section2.style.height = (section2Offset + 2400) / 0.8 + "px";
   }
 
-  if(window.pageYOffset > 1600 && window.pageYOffset < 2135) {
-    section3.style.height = (section3Offset + 3000) / 0.8 + "px";
+  if(window.pageYOffset > 1744 && window.pageYOffset < 2135) {
+    section3.style.height = (section3Offset + 3144) / 0.8 + "px";
   }
 
 
